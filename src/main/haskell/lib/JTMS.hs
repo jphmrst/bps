0f3aaf8798fb335fd07b0e3de643b66ade700dc0
John Maraist <from-github@maraist.org>
2022-02-04 14:43:11 -0600

    Debug links

--- src/main/haskell/lib/JTMS.hs
+++ src/main/haskell/lib/JTMS.hs
@@ -16,9 +16,15 @@ translation of the original code with minimal changes.  Most of the
 deviations from the original are due to either Haskell's strong
 typing, which necessitates some additional tagging, and to the
 abomination which is Lisp's @do@ macro.  The translation relies on
-mutable data structures using `ST` state thread references.  A more
-pure translation, possibly not relying on the `ST` monad/`STT`
-transformer, is a significant piece of future work.
+mutable data structures using `STT` state thread references.  A more
+pure translation, possibly not relying on the [@ST@
+monad]("Control.Monad.ST")/[@STT@
+transformer]("Control.Monad.ST.Trans"), is a significant piece of
+future work.
+
+Note also there are restrictions on the embedded monad @m@ which can
+be wrapped in the `STT` transformer; see [the @Control.Monad.ST.Trans@
+documentation]("Control.Monad.ST.Trans") for details.
 
 See the @LICENSE.txt@ and @README-forbus-dekleer.txt@ files
 distributed with this work for a paragraph stating scope of permission
