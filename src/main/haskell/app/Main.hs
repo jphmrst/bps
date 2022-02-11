915e4dc861b572f7c8d0d145eee23fb650698d3e
John Maraist <from-github@maraist.org>
2022-02-11 13:01:06 -0600

    Start testing actual JTMS artifacts

--- src/main/haskell/app/Main.hs
+++ src/main/haskell/app/Main.hs
@@ -1,6 +1,19 @@
 module Main where
 
-import Lib
+import Data.Symbol
+import Data.TMS.JTMS
+import Control.Monad.IO.Class
 
 main :: IO ()
-main = someFunc
+main =
+  let testSym = intern "test"
+  in do
+    liftIO $ putStrLn $ show testSym
+    runJTMST $ do
+      j <- createJTMS "Ex1"
+      na <- createNode j (intern "a") True False
+      naName <- nodeString na
+      naIn <- isInNode na
+      liftIO $ putStrLn $
+        "Node " ++ naName ++ " is " ++ if naIn then "in" else "out"
+    return ()
