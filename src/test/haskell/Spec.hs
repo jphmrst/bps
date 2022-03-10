dab80bf9cee0563150d160cd65d3b2f3bbda3f4d
John Maraist <from-github@maraist.org>
2022-03-09 22:17:35 -0600

    Update comment

--- src/test/haskell/Spec.hs
+++ src/test/haskell/Spec.hs
@@ -1,5 +1,5 @@
 {-|
-Description : Testing Justification-based truth maintenance systems (JTMSes)
+Description : Testing truth maintenance systems (TMSes)
 Copyright   : (c) John Maraist, 2022
               Kenneth D. Forbus, Johan de Kleer and Xerox Corporation, 1986-1993
 License     : AllRightsReserved
@@ -7,23 +7,8 @@ Maintainer  : haskell-tms@maraist.org
 Stability   : experimental
 Portability : POSIX
 
-Translation of Forbus and de Kleer's justification-based truth
-maintenance systems (JTMSes) from Common Lisp to Haskell.
-
-This is not a very \"Haskelly\" implementation; rather, it is a
-translation of the original code with minimal changes.  Most of the
-deviations from the original are due to either Haskell's strong
-typing, which necessitates some additional tagging, and to the
-abomination which is Lisp's @do@ macro.  The translation relies on
-mutable data structures using `STT` state thread references.  A more
-pure translation, possibly not relying on the [@ST@
-monad]("Control.Monad.ST")/[@STT@
-transformer]("Control.Monad.ST.Trans"), is a significant piece of
-future work.
-
-Note also there are restrictions on the embedded monad @m@ which can
-be wrapped in the `STT` transformer; see [the @Control.Monad.ST.Trans@
-documentation]("Control.Monad.ST.Trans") for details.
+Testing the translation of Forbus and de Kleer's various truth
+maintenance systems (TMSes) from Common Lisp to Haskell.
 
 See the @LICENSE.txt@ and @README-forbus-dekleer.txt@ files
 distributed with this work for a paragraph stating scope of permission
