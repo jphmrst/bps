c07e2d054348f6cc4fea99e2acdabb73400c3501
John Maraist <from-github@maraist.org>
2022-03-10 10:35:47 -0600

    Add further steps

--- src/main/haskell/app/ATMSTrun.hs
+++ src/main/haskell/app/ATMSTrun.hs
@@ -17,6 +17,14 @@ runATMS1 = do
     ne <- createNode atms "E" True False
     -- debugAtms "Added assumption node E" atms
     nh <- createNode atms "H" False False
-    debugAtms "Added non-assuption node H" atms
+    -- debugAtms "Added non-assuption node H" atms
     justifyNode "R1" nh [nc, ne]
-    debugAtms "After rule R1" atms
+    -- debugAtms "After rule R1" atms
+    ng <- createNode atms "G" False False
+    -- debugAtms "After non-assumption node G" atms
+    justifyNode "R2" ng [na, nc]
+    -- debugAtms "After rule R2" atms
+    nx <- createNode atms "X" False True
+    debugAtms "After contradiction node X" atms
+    justifyNode "R3" nx [ng]
+    debugAtms "After rule R3" atms
